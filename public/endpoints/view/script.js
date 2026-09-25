a89570039eb4cc88f0721416d1446f4a0e1daad1
Dinis Duarte <dinisduarte220@gmail.com>
2026-09-25 20:02:37 +0100

    Plane animation between API updates Settings WIndow and checkbox limitations handled MISSING: Update radar on settings apply. They currently are only being stored in localStorage

--- public/endpoints/view/script.js
+++ public/endpoints/view/script.js
@@ -5,7 +5,7 @@ const viewMode = pathParts[2]
 
 let map
 
-let lon = 0, lat = 0, dist = 25
+let lon = 0, lat = 0, dist
 initializeView()
 
 const existingFlights = new Map()
@@ -75,7 +75,10 @@ async function initializeView() {
       .addTo(map);
   }
 
-  const updateTimer = 5000
+  const settings = JSON.parse(localStorage.getItem('settings'))
+  dist = settings.radar_radius
+
+  const updateTimer = 4500
   if (lon != null && lat != null) {
     getFlights()
     setInterval(async () => {
@@ -113,6 +116,8 @@ async function getFlights() {
         // flight.alt_baro == "ground" ||
         flight.seen > 180
       ) return
+      
+      const currentFlight = existingFlights.get(flight.hex)
       flights.push({
         icao: flight.hex,    // Unique identifier
         callsign: flight.flight?.trim(),    // Callsign
@@ -126,8 +131,10 @@ async function getFlights() {
         rate: flight.baro_rate,    // Vertical Rate
         heading: flight.true_heading,    // Heading
         track: flight.track,    // Track
-        latitude: flight.lat,    // Latitude
-        longitude: flight.lon,    // Longitude
+        latitude: currentFlight ? currentFlight.target_latitude : flight.lat,    // Latitude
+        longitude: currentFlight ? currentFlight.target_longitude : flight.lon,    // Longitude
+        target_latitude: flight.lat,
+        target_longitude: flight.lon,
         squawk: flight.squawk,    // Squawk
         last_seen: flight.seen    // Last Update
       })
@@ -136,9 +143,6 @@ async function getFlights() {
     // Compare both Map and Flights Array:
     // Add new flights, update existing and remove old ones
     flights.forEach(element => {
-      if (!existingFlights.has(element.icao)) {
-        existingFlights.set(element.icao, element)
-      }
       existingFlights.set(element.icao, element)
     })
     existingFlights.forEach((element, icao) => {
@@ -226,6 +230,166 @@ function updateFlights() {
       .addTo(map)
 
       existingMarkers.set(element.icao, newMarker)
+    }
+    if (existingMarkers.has(element.icao)) {
+      const marker = existingMarkers.get(element.icao)
+      animateFlight(
+        marker,
+        element.latitude,
+        element.longitude,
+        element.target_latitude,
+        element.target_longitude,
+        4500
+      )
+    }
+  })
+}
+
+// Flights Animation
+function animateFlight(marker, startLat, startLon, targetLat, targetLon, duration) {
+  const startTime = performance.now()
+
+  function animate(currentTime) {
+    const progress = Math.min((currentTime - startTime) / duration, 1)
+
+    const lat = startLat + (targetLat - startLat) * progress
+    const lon = startLon + (targetLon - startLon) * progress
+
+    marker.setLngLat([lon, lat])
+
+    if (progress < 1) {
+      requestAnimationFrame(animate)
+    }
+  }
+
+  requestAnimationFrame(animate)
+}
+
+
+if (!localStorage.getItem('settings')) {
+  const defaultSettings = {
+    "radar_radius": 25,
+    "planes_icon": "icon",
+    "show_marker": true,
+    "label_info": ["callsign", "model", "selected_altitude"]
+  }
+
+  localStorage.setItem('settings', JSON.stringify(defaultSettings))
+}
+
+// Settings
+const settingsWidget = document.getElementById('settingsBar')
+function showSettings() {
+  if (settingsWidget.style.display == "none") {
+    settingsWidget.style.display = "flex"
+    const settings = JSON.parse(localStorage.getItem('settings'))
+
+    settingsWidget.querySelector('#radar_range').value = settings.radar_radius
+    settingsWidget.querySelector('#planes_icon').value = settings.planes_icon
+    settingsWidget.querySelector('#checkbox_showLocation').checked = settings.show_marker
+
+    settings.label_info.forEach(element_setting => {
+      settingsWidget.querySelector(`#label_${element_setting}`).checked = true
+    });
+    verifyCheckBoxes()
+  } else {
+    settingsWidget.style.display = "none"
+  }
+}
+// Limit the Labels info to 3 options
+function verifyCheckBoxes() {
+  const availableCheckBoxs = ["callsign", "route", "altitude", "selected_altitude", "speed", "squawk", "model", "registration", "rate"]
+  const limit = 3
+  let counter = 0
+  const settingsWidget = document.getElementById('settingsBar')
+
+  availableCheckBoxs.forEach(checkbox => {
+    if (settingsWidget.querySelector(`#label_${checkbox}`).checked == true) counter++
+    if (counter == limit) {
+      availableCheckBoxs.forEach(checkBoxToDisable => {
+        if (settingsWidget.querySelector(`#label_${checkBoxToDisable}`).checked == false) settingsWidget.querySelector(`#label_${checkBoxToDisable}`).disabled = true
+      })
+    } else {
+      availableCheckBoxs.forEach(checkBoxToDisable => {
+        if (settingsWidget.querySelector(`#label_${checkBoxToDisable}`).checked == false) settingsWidget.querySelector(`#label_${checkBoxToDisable}`).disabled = false
+      })
+    }
+  })
+}
+// Save Settings
+function saveSettings() {
+  const settingsWidget = document.getElementById('settingsBar')
+  const availableCheckBoxs = ["callsign", "route", "altitude", "selected_altitude", "speed", "squawk", "model", "registration", "rate"]
+  let labelsInfo = []
+
+  availableCheckBoxs.forEach(element_setting => {
+    if (settingsWidget.querySelector(`#label_${element_setting}`).checked == true) labelsInfo.push(element_setting)
+  });
+
+  const newSettings = {
+    "radar_radius": settingsWidget.querySelector('#radar_range').value,
+    "planes_icon": settingsWidget.querySelector('#planes_icon').value,
+    "show_marker": settingsWidget.querySelector('#checkbox_showLocation').checked,
+    "label_info": labelsInfo
+  }
+  
+  localStorage.setItem('settings', JSON.stringify(newSettings))
+
+  settingsWidget.style.display = "none"
+}
+
+function updateRadar() {
+  const settings = JSON.parse(localStorage.getItem('settings'))
+
+
+}
+
+// Radius
+let radiusTimeout
+const radiusTimer = 1500    // 1.5 Seconds
+function drawRadius(radius) {
+
+  clearTimeout(radiusTimeout)
+
+  radiusTimeout = setTimeout(() => {
+    if (map.getLayer("radar_radius")) map.removeLayer("radar_radius")
+    if (map.getLayer("radar_radius_outline")) map.removeLayer("radar_radius_outline")
+    if (map.getSource("radar_radius")) map.removeSource("radar_radius")
+    
+    return
+  }, radiusTimer);
+  const options = {
+    steps: 85,
+    units: "nauticalmiles"
+  }
+  const circle = turf.circle([lon, lat], radius, options)
+
+  if (map.getSource("radar_radius")) {
+    map.getSource("radar_radius").setData(circle)
+    return
+  }
+
+  map.addSource("radar_radius", {
+    type: "geojson",
+    data: circle
+  })
+
+  map.addLayer({
+    id: "radar_radius",
+    type: "fill",
+    source: "radar_radius",
+    paint: {
+      "fill-color": "#8CCFFF",
+      "fill-opacity": 0.5
+    }
+  })
+  map.addLayer({
+    id: "radar_radius_outline",
+    type: "line",
+    source: "radar_radius",
+    paint: {
+      "line-color": "#0094ff",
+      "line-width": 3
     }
   })
 }
